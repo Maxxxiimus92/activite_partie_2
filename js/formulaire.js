b7b16bbe3bd567a09005c05065d2548b2497867b
Maxxxiimus92 <Maxxxiimus92@users.noreply.github.com>
2017-05-24 14:57:58 +0200

    Ajout javascript texte d'aide
    fonctionne avec aide.htm

--- js/formulaire.js
+++ js/formulaire.js
@@ -1,6 +1,9 @@
 $(function(){
 	$('#btn1').click(function(){
-		$('hr').after('<div id="conteneur">Texte du label <input type="text" id="tex"><button id="btnok">OK</button><button id="btnannuler">Annuler</button></div>');
+		/* L'intégration du texte d'aide dans la partie droite de l'écran doit se faire via une balise <div>, insérée dans les trois méthodes after() */
+		$('hr').after('<div id="conteneur">Texte du label <input type="text" id="tex"><button id="btnok">OK</button><button id="btnannuler">Annuler</button><div id="consigne"></div></div>');
+		/* L'affichage du texte d'aide se fait juste après la méthode after() pour chacun des boutons. Pour le bouton 1 */
+		$('#consigne').load('aide.htm #aideLabel');
 		$('.btnprincipal').attr('disabled','disabled');
 		$('#conteneur').hide().fadeIn(2000);
 		$('#tex').focus();
@@ -21,7 +24,8 @@ $(function(){
 	});
 
 	$('#btn2').click(function(){
-		$('hr').after('<div id="conteneur">id de la zone de texte <input type="text" id="tex"><button id="btnok">OK</button><button id="btnannuler">Annuler</button></div>');
+		$('hr').after('<div id="conteneur">id de la zone de texte <input type="text" id="tex"><button id="btnok">OK</button><button id="btnannuler">Annuler</button><div id="consigne"></div></div>');
+		$('#consigne').load('aide.htm #aideZoneTexte');
 		$('.btnprincipal').attr('disabled','disabled');
 		$('#conteneur').hide().fadeIn(2000);
 		$('#tex').focus();
@@ -33,10 +37,17 @@ $(function(){
 				$('.btnprincipal').removeAttr('disabled');
 			});
 		});
+		$('#btnannuler').click(function(){
+			$('#conteneur').fadeOut(2000,function(){
+				$('#conteneur').remove();
+				$('.btnprincipal').removeAttr('disabled');
+			});
+		});  
 	});
 
 	$('#btn3').click(function(){
-		$('hr').after('<div id="conteneur">Texte du bouton <input type="text" id="tex"><button id="btnok">OK</button><button id="btnannuler">Annuler</button></div>');
+		$('hr').after('<div id="conteneur">Texte du bouton <input type="text" id="tex"><button id="btnok">OK</button><button id="btnannuler">Annuler</button><div id="consigne"></div></div>');
+		$('#consigne').load('aide.htm #aideBouton');
 		$('.btnprincipal').attr('disabled','disabled');
 		$('#conteneur').hide().fadeIn(2000);
 		$('#tex').focus();
@@ -53,6 +64,6 @@ $(function(){
 				$('#conteneur').remove();
 				$('.btnprincipal').removeAttr('disabled');
 			});
-		}); 
+		});  
 	});
 });
