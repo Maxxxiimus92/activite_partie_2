6b2eafd65db936633cab6ff655e21a3179d8f09f
Maxxxiimus92 <Maxxxiimus92@users.noreply.github.com>
2017-05-24 14:54:15 +0200

    Ajout bouton annuler au formulaire

--- js/formulaire.js
+++ js/formulaire.js
@@ -1,6 +1,6 @@
 $(function(){
 	$('#btn1').click(function(){
-		$('hr').after('<div id="conteneur">Texte du label <input type="text" id="tex"><button id="btnok">OK</button></div>');
+		$('hr').after('<div id="conteneur">Texte du label <input type="text" id="tex"><button id="btnok">OK</button><button id="btnannuler">Annuler</button></div>');
 		$('.btnprincipal').attr('disabled','disabled');
 		$('#conteneur').hide().fadeIn(2000);
 		$('#tex').focus();
@@ -11,11 +11,17 @@ $(function(){
 				$('#conteneur').remove();
 				$('.btnprincipal').removeAttr('disabled');Me
 			});
-		}); 
+		});
+		$('#btnannuler').click(function(){
+			$('#conteneur').fadeOut(2000,function(){
+			$('#conteneur').remove();
+			$('.btnprincipal').removeAttr('disabled');
+			});
+		});  
 	});
 
 	$('#btn2').click(function(){
-		$('hr').after('<div id="conteneur">id de la zone de texte <input type="text" id="tex"><button id="btnok">OK</button></div>');
+		$('hr').after('<div id="conteneur">id de la zone de texte <input type="text" id="tex"><button id="btnok">OK</button><button id="btnannuler">Annuler</button></div>');
 		$('.btnprincipal').attr('disabled','disabled');
 		$('#conteneur').hide().fadeIn(2000);
 		$('#tex').focus();
@@ -30,13 +36,19 @@ $(function(){
 	});
 
 	$('#btn3').click(function(){
-		$('hr').after('<div id="conteneur">Texte du bouton <input type="text" id="tex"><button id="btnok">OK</button></div>');
+		$('hr').after('<div id="conteneur">Texte du bouton <input type="text" id="tex"><button id="btnok">OK</button><button id="btnannuler">Annuler</button></div>');
 		$('.btnprincipal').attr('disabled','disabled');
 		$('#conteneur').hide().fadeIn(2000);
 		$('#tex').focus();
 		$('#btnok').click(function(){
 			var lid =  '<button>' + $('#tex').val() + '</button>';
 			$('#gauche').append(lid);
+			$('#conteneur').fadeOut(2000,function(){
+				$('#conteneur').remove();
+				$('.btnprincipal').removeAttr('disabled');
+			});
+		});
+		$('#btnannuler').click(function(){
 			$('#conteneur').fadeOut(2000,function(){
 				$('#conteneur').remove();
 				$('.btnprincipal').removeAttr('disabled');
